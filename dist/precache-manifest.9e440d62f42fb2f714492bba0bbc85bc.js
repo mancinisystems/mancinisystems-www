a10a91629ea0fc1f378dfad8d3bed985ed2425fb
Todd A. Mancini <todd.mancini@daxat.com>
2018-07-04 12:40:40 -0400

    prod build

---
+++ dist/precache-manifest.9e440d62f42fb2f714492bba0bbc85bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6a5d91bee4615c0e2f8",
+    "url": "/js/chunk-vendors.d6a5d91b.js"
+  },
+  {
+    "revision": "208202936762f2160f58",
+    "url": "/js/app.20820293.js"
+  },
+  {
+    "revision": "055e7d0b51828a7ff7f01edc0880b116",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "208202936762f2160f58",
+    "url": "/css/app.ef127b53.css"
+  }
+];
